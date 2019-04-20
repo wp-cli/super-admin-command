873affc4c0abde04fb6050082324401af0c2bf34
Wojciech Smoliński <wojsmol@wp.pl>
2019-04-20 17:25:25 +0200

    phpcs fixes

--- super-admin-command.php
+++ super-admin-command.php
@@ -9,10 +9,14 @@ if ( file_exists( $wpcli_super_admin_autoloader ) ) {
 	require_once $wpcli_super_admin_autoloader;
 }
 
-WP_CLI::add_command( 'super-admin', 'Super_Admin_Command', array(
-	'before_invoke' => function () {
-		if ( !is_multisite() ) {
-			WP_CLI::error( 'This is not a multisite installation.' );
-		}
-	}
-) );
+WP_CLI::add_command(
+	'super-admin',
+	'Super_Admin_Command',
+	array(
+		'before_invoke' => function () {
+			if ( ! is_multisite() ) {
+				WP_CLI::error( 'This is not a multisite installation.' );
+			}
+		},
+	)
+);
